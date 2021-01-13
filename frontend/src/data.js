1cc3ff7cefb75e399a24d4daf872fee49db84544
andrem91 <andrem166@gmail.com>
2021-01-13 07:49:17 -0300

    share code on github

---
+++ frontend/src/data.js
@@ -0,0 +1,71 @@
+const data = {
+  products: [
+    {
+      _id:'1',
+      name: 'Os segredos da mente milionária',
+      category: 'Finanças',
+      image: '/images/os-segredos-da-mente-milionaria.jpg',
+      price: 27,
+      author: 'T. Harv Eker',
+      rating: 5,
+      numReviews: 10,
+      description:'Aprenda a enriquecer mudando seus conceitos sobre o dinheiro e adotando os hábitos das pessoas bem-sucedidas.'
+    },
+    {
+      _id:'2',
+      name: 'Gestão de alta performance',
+      category: 'Gestão',
+      image: '/images/gestao-de-alta-performace.jpg',
+      price: 32,
+      author: 'Andrew S. Grove',
+      rating: 4.0,
+      numReviews: 8,
+      description:'Considerado um clássico entre os livros de negócios e obra de cabeceira de importantes líderes da atualidade , Gestão de alta performance ensina tudo o que um gestor precisa saber sobre como criar e sustentar uma empresa de alto rendimento.'
+    },
+    {
+      _id:'3',
+      name: 'A Revolução dos Bihos',
+      category: 'Romance',
+      image: '/images/a-revolucao-dos-bichos.jpg',
+      price: 20,
+      author: 'George Orwell',
+      rating: 4.5,
+      numReviews: 6,
+      description:'Narra a insurreição dos animais de uma granja contra seus donos. Progressivamente, porém, a revolução degenera numa tirania ainda mais opressiva que a dos humanos.'
+    },
+    {
+      _id:'4',
+      name: 'O homem mais rico da Babilônia',
+      category: 'Finanças',
+      image: '/images/o-homem-mais-rico-da-babilonia.jpg',
+      price: 15,
+      author: 'George S Clason',
+      rating: 5.0,
+      numReviews: 3,
+      description:'O homem mais rico da Babilônia é um clássico sobre como multiplicar riqueza e solucionar problemas financeiros. Baseando-se nos segredos de sucesso dos antigos babilônicos.'
+    },
+    {
+      _id:'5',
+      name: '1984',
+      category: 'Romance',
+      image: '/images/1984.jpg',
+      price: 31,
+      author: 'George Orwell',
+      rating: 4.5,
+      numReviews: 13,
+      description:'Publicada originalmente em 1949, a distopia futurista 1984 é um dos romances mais influentes do século XX, um inquestionável clássico moderno.'
+    },
+    {
+      _id:'6',
+      name: 'O monge e o executivo',
+      category: 'Liderança',
+      image: '/images/o-monge-e-o-executivo.jpg',
+      price: 25,
+      author: 'James C. Hunter',
+      rating: 4.0,
+      numReviews: 7,
+      description:'Você está convidado a juntar-se a um grupo que durante uma semana vai estudar com um dos maiores especialistas em liderança dos Estados Unidos.'
+    }
+  ]
+};
+export default data;
